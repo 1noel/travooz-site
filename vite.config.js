84043dc6d2a56deedf2c0eb73ebfa077b9763344
nathanael250 <154541061+nathanael250@users.noreply.github.com>
2025-10-16 06:28:38 +0200

    Configure Vite server settings and allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: true, // binds to 0.0.0.0
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: [
+      'travooz-frontend.onrender.com' // add your Render URL here
+    ]
+  }
 })
